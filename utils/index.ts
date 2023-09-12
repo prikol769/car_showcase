45a22ab5dbf1ac20ba8b336e030d59bf3de09276
prikol769 <prikol769@gmail.com>
2023-09-12 11:56:38 -0500

    added generate car images

--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,4 @@
+import {CarProps} from "@/types";
 
 export const calculateCarRent = (city_mpg: number, year: number) => {
     const basePricePerDay = 50; // Base rental price per day in dollars
@@ -27,4 +28,20 @@ export async function fetchCars(){
     const result = await response.json();
 
     return result;
+}
+
+export const generateCarImageUrl = (car:CarProps, angle?:string) => {
+    const url = new URL('https://cdn.imagin.studio/getimage');
+
+    const {make, year, model} = car;
+
+    url.searchParams.append('customer', 'hrjavascript-mastery');
+    url.searchParams.append('make', make);
+    url.searchParams.append('modelFamily', model.split(" ")[0]);
+    url.searchParams.append('zoomType', 'fullscreen');
+    url.searchParams.append('modelYear', `${year}`);
+    // url.searchParams.append('zoomLevel', zoomLevel);
+    url.searchParams.append('angle', `${angle}`);
+
+    return `${url}`;
 }

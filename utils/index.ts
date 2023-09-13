bfbed723b5730cbce78e291de0d2ecbc60fa72d9
prikol769 <prikol769@gmail.com>
2023-09-13 08:38:21 -0500

    added Custom Filters

--- utils/index.ts
+++ utils/index.ts
@@ -1,4 +1,4 @@
-import {CarProps} from "@/types";
+import {CarProps, FilterProps} from "@/types";
 
 export const calculateCarRent = (city_mpg: number, year: number) => {
     const basePricePerDay = 50; // Base rental price per day in dollars
@@ -14,14 +14,28 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
 
     return rentalRatePerDay.toFixed(0);
 };
-export async function fetchCars(){
+
+
+export const updateSearchParams = (type: string, value: string) => {
+    const searchParams = new URLSearchParams(window.location.search);
+
+    searchParams.set(type, value);
+
+    const newPathname = `${window.location.pathname}?${searchParams.toString()}`;
+
+    return newPathname;
+};
+export async function fetchCars(filters: FilterProps){
+    const {manufacturer, model, year, limit, fuel} = filters;
+
     const headers = {
         'X-RapidAPI-Key': '0752a38cedmshd822e7b0efe69c0p18bc5bjsn3b4120bfee46',
         'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
     }
 
     const response = await fetch(
-        'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla',
+        `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?
+        make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`,
         {headers: headers}
     );
 
